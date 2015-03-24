5387c54ce6483422371d4de742d45ff1c566e832
johnnay <johnnay@thelonious.cas.vanderbilt.edu>
2015-03-24 18:24:59 -0500

    sobol sa bias correction and plotting functions

--- sobol_sa.R
+++ sobol_sa.R
@@ -89,3 +89,27 @@ sobol_sa <- function(abm, input_values,
   sobol_aggregate
 }
 
+correct_bias <- function(x) {
+  x_corr <- x[["original"]] - x[["bias"]]
+  min_ci <- x[["min. c.i."]]
+  max_ci <- x[["max. c.i."]]
+  x <- cbind(x_corr = x_corr, min_ci = min_ci, max_ci = max_ci, x)
+  x[x < 0] <- 0
+  x[x > 1] <- 1
+  cbind(var = row.names(x), x)
+}
+
+plot_sobol_fo <- function(x, outcome_var = "Outcome"){
+  ss <- correct_bias(x$S)
+  ggplot(ss, aes(x = var, y = x_corr)) + geom_point() + xlab("Variable") + ylab("") +
+    ggtitle(paste("First Order Effects of Variables on", outcome_var)) +
+    geom_errorbar(aes(ymax = max_ci, ymin = min_ci), width=0.25)
+}
+
+plot_sobol_total <- function(x, outcome_var = "Outcome"){
+  tt <- correct_bias(x$T)
+  ggplot(tt, aes(x = var, y = x_corr)) + geom_point() + xlab("Variable") + ylab("") +
+    ggtitle(paste("Total Sensitivity Effects of Variables on", outcome_var)) +
+    geom_errorbar(aes(ymax = max_ci, ymin = min_ci), width=0.25)
+}
+
