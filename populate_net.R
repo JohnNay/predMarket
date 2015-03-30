d31026810baec438ae394b5978b3894c0e10b123
Martin Van der Linden <martinvanderlinden@Martins-MacBook-Air.local>
2015-03-29 20:56:08 -0500

    Adapt terminology regarding true model.
    Add attributes to the graph to allow measuring util.converg, and record initial value of util.converg.

--- populate_net.R
+++ populate_net.R
@@ -23,12 +23,32 @@ PopulateNet <- function(
   V(g)$money  <- mon.dis
   V(g)$approx <- approx.dis 
     #NOTE : in the preliminary version, approximate models are taken from Sumner and Jackson (2008) 
-            # approx = 1  ->  true model (aka true)
+            # approx = 1  ->  Full model : ACC true, both geoingeneering and policy effective (aka full)
             # approx = 2  ->  ACC is a myth (aka myth)
             # approx = 3  ->  ACC is true but all measures are ineffective (aka ineff)
             # approx = 4  ->  ACC is true and policy are effective, but not geoingeneering (aka policy)
             # approx = 5  ->  ACC is true, geoingeneering is effective, but policy is not (aka engineer)
+  
+  # Assign values for the distance between approximate models. 
+  
+  V(g)$dist <- matrix(
+                c (0,3,2,1,1,
+                   3,0,1,1,2,
+                   2,3,0,1,1,
+                   1,2,3,0,0,
+                   1,2,3,0,0),
+                nrow=5,
+                ncol=5)
+  
+  # Will be used for
+  # convergence analysis in order to compute some social utility associated
+  # with a distribution of distances to the true model emerging from the 
+  # distribution of approximate models.
+  
+  
   V(g)$behav  <- behav.dis  # Market behavior of the traders, only ZI in preliminary model
+  
+  
   
   risk.tak.dis <- sample(0:(risk.tak*10000)/10000, n.traders, replace = TRUE)
   V(g)$risk.tak <- risk.tak.dis # Tendency of a player to take risk on the market by 
