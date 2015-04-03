2a055f8195b9d38ce337741d31bf027aa24dc545
Martin Van der Linden <martinvanderlinden@Martins-MacBook-Air.local>
2015-04-03 17:50:37 -0500

    source() in right position

--- Test_main_or_sobol.R
+++ Test_main_or_sobol.R
@@ -1,10 +1,9 @@
 # A simple test to give a better idea of whether the problem comes from main or
 # from sobol et al.
 
-source("main.R")
-
-for ( j in 1:20){
+for ( j in 1:10000){
   rm(list=ls()) 
+  source("main.R")
   s <- runif(4, min = 0.0001, max = 0.9999)
   print(s)
   result <- main(parameters = s, out = "converg")
