47847ba9403c455f214b3dd49362cd9379d1d946
John Nay <john@debian.vanderbilt.edu>
2015-04-03 14:55:24 -0500

    error: "subscript out of bounds"

--- main.R
+++ main.R
@@ -40,7 +40,7 @@ main <- function(parameters,
   seg <- parameters[1] # continuous value in (0,1)
   ideo <- parameters[2] # continuous value in (0,1)
   risk.tak <- parameters[3] # continuous value in (0,1)
-  market.complet <- ifelse(parameters[4]*1000 < 1, 1, round(parameters[2]*1000)) # integer in (1, 1000)
+  market.complet <- ifelse(parameters[4]*1000 < 1, 1, round(parameters[4]*1000)) # integer in (1, 1000)
   
   cat(paste("seg", seg, "ideo", ideo, "risk.tak", risk.tak, "market.complet", market.complet, "\n"))
   
