88739db8e01361d64847a9bbd18c9d89d77c6300
John Nay <john@debian.vanderbilt.edu>
2015-11-15 14:08:39 -0600

    test_main updated to test future scenarios

--- test_main.R
+++ test_main.R
@@ -3,5 +3,9 @@
 #
 rm(list = ls())
 options(warn = 2)
-source('main.R')
-x <- main(c(runif(3, min = 0.0001, max = 0.9999), 0, runif(2, min = 0.0001, max = 0.9999)), iterations = 1)
+source('~/Documents/prediction_market/main.R')
+x <- main(c(runif(3, min = 0.0001, max = 0.9999), 0, runif(2, min = 0.0001, max = 0.9999)), iterations = 1,
+          burn.in = 135,
+          n.seq = 14,
+          horizon = 6,
+          nyears = 219)
