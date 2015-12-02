a95d05c4508bb01adf201b03f608637ac1a147b2
JohnNay <john.j.nay@gmail.com>
2015-12-02 12:01:56 -0600

    Added a slow tsi data generation to script.

--- test_main.R
+++ test_main.R
@@ -125,6 +125,30 @@ if(!debug){
     mean.true <- append(mean.true, mean(V(x[[length(x)]])$money[V(x[[length(x)]])$approx == x[[length(x)]]$true.model]))
     mean.false <- append(mean.false, mean(V(x[[length(x)]])$money[V(x[[length(x)]])$approx != x[[length(x)]]$true.model]))
     
+    x <- main(parameters = c(runif(3, min = 0.0001, max = 0.9999), # seg, ideo, risk.tak
+                             0, # true model: 0 = slow.tsi, 1 = log.co2
+                             runif(2, min = 0.0001, max = 0.9999)), # n.edge and n.traders
+              out = "fraction_converge",
+              safeNprint=FALSE,
+              iterations = 1,
+              burn.in = burn.in,
+              n.seq = n.seq,
+              horizon = horizon,
+              nyears = burn.in + n.seq * horizon,
+              record = TRUE,
+              full_history = TRUE,
+              adaptation = adaptation,
+              # saving and loading climate data
+              load_previous = TRUE,
+              saving = TRUE,
+              perfect = TRUE
+    )
+    
+    id <- append(id,iteration)
+    true.model <- append(true.model, x[[length(x)]]$true.model)
+    mean.true <- append(mean.true, mean(V(x[[length(x)]])$money[V(x[[length(x)]])$approx == x[[length(x)]]$true.model]))
+    mean.false <- append(mean.false, mean(V(x[[length(x)]])$money[V(x[[length(x)]])$approx != x[[length(x)]]$true.model]))
+    
   }
   
   money.data <- data.frame(id,true.model,mean.true, mean.false)
