7bd539f87f449aa288788accf529725b148d2682
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2015-12-04 16:45:11 -0600

    Updated plot.

--- poster_plot.R
+++ poster_plot.R
@@ -11,7 +11,7 @@ plot_data$set <- factor(plot_data$set, levels = c("0.05, 0.05", "0.05, 0.95", "0
                         labels = c("105 edges, segmentation = 0.05", "105 edges, segmentation = 0.95", "195 edges, segmentation = 0.05", "195 edges, segmentation = 0.95"))
 
 
-ggplot(data=plot_data, aes(x= trading_seq * 6 + 2014, y=(convergence + 1) * 50, color = true_mod)) +
+p <- ggplot(data=plot_data, aes(x= trading_seq * 6 + 2014, y=(convergence + 1) * 50, color = true_mod)) +
   geom_point(position = position_jitter(w = 0.05, h = 0), alpha = 0.1) + geom_smooth(size=1, fill=NA) +
   facet_wrap(~set, ncol = 1) +
   ggtitle(paste0("Convergence Over Trading Sequences 2015-",  2014 + 14 * 6, " (initial history = 135 years)")) +
@@ -22,3 +22,9 @@ ggplot(data=plot_data, aes(x= trading_seq * 6 + 2014, y=(convergence + 1) * 50, 
   theme_bw() + theme(legend.justification=c(1,0), legend.position=c(1,0)) +
   #scale_color_discrete(name="True Model") +
   scale_color_brewer(palette="Set1", name="True Model", labels = c(LogCo2 = expression(CO[2]), SlowTSI = "TSI"))
+
+print(p)
+
+pdf("output/jg_timeseries_future.pdf", width=8, height=18)
+print(p)
+dev.off()
